582e976d822b377f68f8494b1ec70df9e68c832c
Wesley Childs <childs.wesley@gmail.com>
2011-02-24 22:02:03 +0000

    Enabled add Change models

--- mainsite/models.py
+++ mainsite/models.py
@@ -1,19 +1,19 @@
 from django.db import models
 
 class Project(models.Model):
-    title = models.CharField()
-    summary = models.CharField()
+    title = models.CharField(max_length="100")
+    summary = models.TextField()
     active = models.BooleanField()
-    slug = models.CharField()
+    slug = models.CharField(max_length="20")
     
     
 class ProjectGoals(models.Model):
     project = models.ForeignKey(Project)
-    item = models.CharField()
+    item = models.CharField(max_length="100")
     
 
 class ProjectContent(models.Model):
     project = models.ForeignKey(Project)
-    content = models.CharField()
+    content = models.TextField()
     
     
