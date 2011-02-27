1dc43769bd4bb9865fe1b22f72bc8c2da69ca17b
Wesley Childs <childs.wesley@gmail.com>
2011-02-27 18:16:49 +0000

    Stylesheets

--- urls.py
+++ urls.py
@@ -1,9 +1,13 @@
+import os
+
 from django.conf.urls.defaults import *
 
 from mainsite.views import *
 
 from django.contrib import admin
 admin.autodiscover()
+
+DIRNAME = os.path.dirname(__file__)
 
 urlpatterns = patterns('',
     # Example:
@@ -14,4 +18,6 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
+    (r'^static/(?P<path>.*)$','django.views.static.serve',
+     {'document_root': os.path.join(DIRNAME, "static"), 'show_indexes': True}),
 )
